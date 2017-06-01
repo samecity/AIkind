50f4deff10af1dc9ae8f16a0efccb6d9afc82edd
AIbeing <iszanf@icloud.com>
2017-06-01 20:04:16 +0800

    Update init.js

--- core/init.js
+++ core/init.js
@@ -1,1 +1,29 @@
+function $(element,obj){
+	this.element = document.createElement(element);
+	return this;
+}
+//HTML DOM element Operator;
+$.prototype = {
+	element: NULL,
+	ids:[],
+	tags:[],
+	id:function(id){
+		this.ids[] = this.element = document.getElementById(id);
+		return this;
+	},
+	tag:function(tag){
+		this.tags[] = this.element = document.getElementByTagName(tag);
+		return this;
+	}
+	set:function(obj){
+		for(let type in obj){
+			this.element.setAttribute(type,obj[type]);
+		}
+		return this;
+	}
+}
 
+function request(path){
+	var script = $('script').set({'type':'text/javascript','src':path});
+	$.tag('head').element[0].appendChild(script);
+}
