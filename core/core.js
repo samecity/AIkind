2fb401ace0d2fe177370870bebc8c20dd9a2e8f5
AIbeing <iszanf@icloud.com>
2017-06-27 12:43:52 +0800

    Update core.js

--- core/core.js
+++ core/core.js
@@ -1,7 +1,23 @@
 function $class(obj){
-  var _class = obj.init;
-  for(let key in obj){
-    _class.prototype[key] = obj[key];
-  }
-  return _class;
+	var _class = obj.init;
+	for(let key in obj){
+		_class.prototype[key] = obj[key];
+	}
+	return _class;
 }
+
+var $ = new $class({
+	element:'',
+	ids:[],
+	tags:[],
+	nodes:[],
+	doms:[],
+	init:function(){
+		var opra
+		switch(){
+			case '.': 
+			   this.element = (typeof )
+		}
+	},
+	
+})();
