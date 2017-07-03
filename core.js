f61039923b18053a1f7233fd66864da87d85416e
AIbeing <iszanf@icloud.com>
2017-07-03 16:32:25 +0800

    Update core.js

--- core.js
+++ core.js
@@ -1,8 +1,16 @@
 var ai = new (_class(){
+	tweak_amount:0.01,
 	init:function(){
 		return this;
 	},
-	forwardmultiplygate:function(x,y){
+	forward_multiply_gate:function(x,y){
 		return x*y;
 	},
+	derivative:function(){
+		var x = -2, y = 3;
+		var out_xmy = this.forward_multiply_gate(x,y);
+		var xph = x + this.tweak_amount;
+		var out_xphmy = this.forward_multiply_gate(xph,y); 
+		var x_derivative = (out_xphmy - out_xmy)/h;
+	}
 })();
